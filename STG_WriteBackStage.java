5fabe79d2d2dcf883994d356457619506b9caf12
Ebrahim Asadi <asadi@users.noreply.github.com>
2022-03-09 20:57:27 +0330

    Update STG_WriteBackStage.java

--- STG_WriteBackStage.java
+++ STG_WriteBackStage.java
@@ -1,15 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package ebrahim.asadi.fum.mips;
-
 /**
  *
- * @author Ebrahim Asadi (asadi.email@gmail.com)
+ * @author Ebrahim Asadi (ceo@hdfsoft.com)
  * 
  */
+
+package ebrahim.asadi.fum.mips;
+
 public class STG_WriteBackStage extends STG_Stage
 {
 
