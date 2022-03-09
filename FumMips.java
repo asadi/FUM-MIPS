fb572c6cb426b025644af97a7f1dba0206024045
Ebrahim Asadi <asadi@users.noreply.github.com>
2022-03-09 20:52:09 +0330

    Update FumMips.java

--- FumMips.java
+++ FumMips.java
@@ -1,17 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author Ebrahim Asadi (ceo@hdfsoft.com)
+ * 
  */
+
 package ebrahim.asadi.fum.mips;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author Ebrahim Asadi (asadi.email@gmail.com)
- * 
- */
 public class FumMips {
 
     protected final RegisterBank registerBank;
