82c89ed11814596d294689b57f0b8222e7819f8e
Ebrahim Asadi <asadi@users.noreply.github.com>
2022-03-09 20:54:59 +0330

    Update RegisterBank.java

--- RegisterBank.java
+++ RegisterBank.java
@@ -1,17 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+ * @author Ebrahim Asadi (ceo@hdfsoft.com)
+ * 
  */
+
 package ebrahim.asadi.fum.mips;
 
 import java.util.ArrayList;
-
-/**
- *
- * @author Ebrahim Asadi (asadi.email@gmail.com)
- * 
- */
 
 public class RegisterBank {
   //  private Register[] registers;
